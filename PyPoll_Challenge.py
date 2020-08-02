2b93eb50c1fe6fbe357adbdacd3e4507ae2fa1d6
bdeluna16 <67936161+bdeluna16@users.noreply.github.com>
2020-08-02 14:46:47 -0700

    Adding election_results and updating readme

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("../Resources/election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -18,7 +18,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
+candidate_counties = []
+candidate_counties_votes = {}
 
 
 # Track the winning candidate, vote count and percentage
@@ -27,7 +28,9 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
+winning_county = ""
+winning_county_count = 0
+winning_county_percentage = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -47,7 +50,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,16 +67,16 @@ with open(file_to_load) as election_data:
 
         # 4a: Write a decision statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_name not in candidate_counties:
 
             # 4b: Add the existing county to the list of counties.
-
+            candidate_counties.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            candidate_counties_votes[county_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
+        candidate_counties_votes[county_name] += 1
 
 
 # Save the results to our text file.
@@ -91,25 +94,32 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a repetition statement to get the county from the county dictionary.
-
+    for county_name in candidate_counties_votes:
         # 6b: Retrieve the county vote count.
-
+        votes = candidate_counties_votes[county_name]
         # 6c: Calculate the percent of total votes for the county.
-
-
+        voter_percentage = float(votes) / (total_votes) * 100 
+        county_results = (
+            f"{county_name}: {voter_percentage:.1f}% ({votes:,})\n")
          # 6d: Print the county results to the terminal.
-
+        print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write a decision statement to determine the winning county and get its vote count.
-
+        if (votes > winning_county_count) and (voter_percentage > winning_percentage):
+            winning_county_count = votes
+            winning_county = county_name
+            winning_county_percentage = voter_percentage
 
     # 7: Print the county with the largest turnout to the terminal.
-
-
+    winning_county = (
+        f"-------------------------\n"
+        f"Largest County Turnout: {winning_county}\n"
+        f"-------------------------\n")
+    print(winning_county, end="")
     # 8: Save the county with the largest turnout to a text file.
-
-
+    txt_file.write(winning_county)
+   
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
 
@@ -139,6 +149,5 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
-
     # Save the winning candidate's name to the text file
     txt_file.write(winning_candidate_summary)
--- README.md
+++ README.md
@@ -24,5 +24,39 @@ The analysis of the election show that:
   * Raymon Anthony Doane recieved 3.1% of the vote with 11,607 number of votes.
 * The winner of the election was:
   * Diana DeGette who received 73.8% of the vote with 272,892 number of votes.
-  
 
+## Challenge Overview
+The Colorado Board of Elections has requested additional data to complete their congressional election audit. They have requested the information below:
+  1. The voter turnout for each county.
+  2. The percentage of votes from each county out of the total count.
+  3. The county with the highest turnout.
+
+## Challenge Summary
+ * There was a total of 369,711 votes in the congressional election
+ * The voter base for the election was comprised of 3 counties.
+ * The results below show the number of votes and perecentage of total votes of the election for each of the 3 counties.
+
+ ![image](https://user-images.githubusercontent.com/67936161/89132957-c1ea5c00-d4cc-11ea-87fc-9adeca2d2ef8.png)
+
+* The data shows that Denver county had the largest number of votes
+*The results below show the number of votes each candidate received in the election and the perecentage of the total votes they received
+
+![image](https://user-images.githubusercontent.com/67936161/89133052-88feb700-d4cd-11ea-9ea1-af441ddda4db.png)
+
+* The data shows that Diana DeGette was the winner of the election. She recevied 272,892 votes which made up 73.8% of the total votes
+
+### Summary Statement
+The script that I made for this election audit can be used for any election. However, in order to use this for other elections some modifaction will need to be made.
+See below for examples of modifications:
+  1. The code to set the variables used to output the results in the analyis can be changed to whatever is necessary. 
+  2. The code to create the lists/dictionaries of the metrics being measured can be changed as well.
+``` 
+  Candidate Options and candidate votes.
+  candidate_options = []
+  candidate_votes = {}
+
+  Create a county list and county votes dictionary.
+  candidate_counties = []
+  candidate_counties_votes = {}
+
+```
